bed8bd7dd1040d96cd97250b541cbc62bc319a6b
QAQ-OWO <lwq2033993788@gmail.com>
2026-08-17 06:22:02 -0600

    feat: 初始化 Spring Boot 4.1 项目骨架

---
+++ src/main/java/com/mymedia/MyMediaApplication.java
@@ -0,0 +1,12 @@
+package com.mymedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMediaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyMediaApplication.class, args);
+    }
+}
